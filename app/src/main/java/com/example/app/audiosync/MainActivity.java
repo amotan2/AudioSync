32b96453f3a06367dda8c818443641975f0a454f
ABDULLAH MOTAN <abdullah.motan@gmail.com>
2017-01-08 23:57:12 +0500

    Logo added Splash Screen added (Not implemented) Login XML and Fragment added (with event listeners) Color Scheme added Button design

--- app/src/main/java/com/example/app/audiosync/MainActivity.java
+++ app/src/main/java/com/example/app/audiosync/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.app.audiosync;
 
+import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -7,7 +9,14 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //setTheme(R.style.splashScreenTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Calling the Login fragment from the main activity
+        Fragment newFrag = new Fragment();
+        FragmentTransaction ft = getFragmentManager().beginTransaction();
+        ft.add(R.id.login_fragment, newFrag);
+        ft.commit();
     }
 }
